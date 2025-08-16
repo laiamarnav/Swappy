05c20d6f6f84a7b695516abb6ac13784dc68c802
laiamarnav <124734800+laiamarnav@users.noreply.github.com>
2025-08-16 19:56:46 +0200

    Add splash screen and set as initial route

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
-import 'infrastructure/di/locator.dart';
-import 'firebase_options.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+
 import 'application/app_state_controller.dart';
+import 'firebase_options.dart';
+import 'infrastructure/di/locator.dart';
+import 'presentation/splash/splash_screen.dart';
+import 'routes.dart';
+import 'ui/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +20,32 @@ void main() async {
   runApp(
     ChangeNotifierProvider<AppStateController>.value(
       value: locator<AppStateController>(),
-      child: const MyApp(),
+      child: const SwappyApp(),
     ),
   );
+}
+
+class SwappyApp extends StatelessWidget {
+  const SwappyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
+      debugShowCheckedModeBanner: false,
+      theme: appTheme(),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('es'),
+      ],
+      home: const SplashScreen(),
+      routes: appRoutes,
+    );
+  }
 }
